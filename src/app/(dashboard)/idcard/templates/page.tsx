5eccd50e29608ae2d8c6931dd5b8c3f8e3f36b6b
cecep1105 <cecep1105@gmail.com>
2026-08-03 21:51:03 +0700

    modified:   src/app/(dashboard)/idcard/templates/page.tsx

--- src/app/(dashboard)/idcard/templates/page.tsx
+++ src/app/(dashboard)/idcard/templates/page.tsx
@@ -54,7 +54,6 @@ export default async function IdCardTemplatesPage() {
                   </TableCell>
                   <TableCell>
                     <div className="flex justify-end">
-                      {resolveMediaUrl(tmpl.background_image)}
                       <TemplateActionsMenu template={tmpl} />
                     </div>
                   </TableCell>
